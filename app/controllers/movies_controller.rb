8ff5d6245d02952c0f25ba63615ece5792d453ef
Samuel Liu <samuelfxliu@berkeley.edu>
2021-10-13 01:14:38 +0000

    part 1 work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,6 +37,10 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def all_ratings
+    @all_ratings = Movie.all_ratings()
+  end
 
   private
   # Making "internal" methods private is not required, but is a common practice.
