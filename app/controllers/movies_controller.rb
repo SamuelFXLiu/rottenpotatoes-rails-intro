40b6aeab9b18bf56dcd946259329ed139fec167d
Samuel Liu <samuelfxliu@berkeley.edu>
2021-10-13 04:23:21 +0000

    part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,6 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings
     @ratings_to_show = []
-    puts params
     if (params.include?(:ratings))
       if (params[:ratings].length > 0)
       @ratings_to_show = params[:ratings].keys
